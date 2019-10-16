576366832b68f3b62c367d536795d7f3245b0813
MosaKasem <thescorpion119@gmail.com>
2019-10-16 18:50:46 +0100

    tutoring session completed, questions were asked

--- src/kr222if/graphs/impl/MyGraph.java
+++ src/kr222if/graphs/impl/MyGraph.java
@@ -11,112 +11,203 @@ import kr222if.graphs.Node;
 
 public class MyGraph<T> implements DirectedGraph<T> {
     private List<Node<T>> nodes;
-    private int counter = 0;
+    private int Headcount = 0;
     public MyGraph() {
         nodes = new ArrayList<Node<T>>();
     }
 
 	@Override
 	public Node<T> addNodeFor(T item) {
+        MyNode<T> node = null;
         if (item == null) {
             throw new NullPointerException("item does not contain any value!");
         }
-        MyNode<T> node = null;
         if (containsNodeFor(item)) {
-            return nodes.get(counter);
+            return nodes.get(Headcount);
         } else {
             node = new MyNode<T>(item);
             nodes.add(node);
         }
+        System.out.println("Nodes :" + node);
 		return node;
 	}
 
 	@Override
 	public Node<T> getNodeFor(T item) {
-		if (item == null || !nodes.contains(item)) {
+		if (item == null) {
             throw new NullPointerException("item does not contain any value!");
-        } else {
-            return nodes.get(counter);
         }
 
+        Iterator<Node<T>> n = nodes.iterator();
+        while (n.hasNext()) {
+            Node<T> node = n.next();
+            if (node.item().equals(item)) {
+                return node;
+            }
+        }
+        return null;
 	}
 
 	@Override
 	public boolean addEdgeFor(T from, T to) {
-		// TODO Auto-generated method stub
-		return false;
+        if (from == null || to == null) {
+            throw new NullPointerException("input does not contain any value!");
+        }
+        if (containsEdgeFor(from, to)) {
+            return false;
+        }
+        Node<T> nodePred = addNodeFor(from);
+        Node<T> nodeSucc = addNodeFor(to);
+        ((MyNode<T>) nodePred).addSucc(nodeSucc);
+        ((MyNode<T>) nodeSucc).addPred(nodePred);
+		return true;
 	}
 
 	@Override
 	public boolean containsNodeFor(T item) {
-		// TODO Auto-generated method stub
+        if (item == null) {
+            throw new NullPointerException("item does not contain any value!");
+        }
+        Iterator<Node<T>> n = nodes.iterator();
+        while (n.hasNext()) {
+            if (n.next().item().equals(item)) {
+                return true;
+            }
+        }
 		return false;
 	}
 
 	@Override
 	public void removeNodeFor(T item) {
-		// TODO Auto-generated method stub
-		
+        Node<T> theNode = getNodeFor(item);
+        if (theNode != null) {
+            nodes.remove(theNode);
+        }
 	}
 
 	@Override
 	public boolean containsEdgeFor(T from, T to) {
-		// TODO Auto-generated method stub
+        Node<T> nodeFrom = this.getNodeFor(from);
+        Node<T> nodeTo = this.getNodeFor(to);
+		if (nodeFrom != null && nodeTo != null) {
+            if (nodeFrom.hasSucc(nodeTo) && nodeTo.hasPred(nodeFrom)) {
+                return true;
+            }
+        }
 		return false;
 	}
 
 	@Override
 	public boolean removeEdgeFor(T from, T to) {
-		// TODO Auto-generated method stub
+        Node<T> nodeFrom = this.getNodeFor(from);
+        Node<T> nodeTo = this.getNodeFor(to);
+        if (nodeFrom != null && nodeTo != null) { 
+            if (nodeFrom.hasSucc(nodeTo) && nodeTo.hasPred(nodeFrom)) {
+                ((MyNode<T>) nodeFrom).removeSucc(nodeTo);
+                ((MyNode<T>) nodeTo).removePred(nodeFrom);
+                return true;
+            }
+        }
 		return false;
 	}
 
     @Override
     public int nodeCount() {
-        // TODO Auto-generated method stub
-        return 0;
+        return nodes.size();
     }
 
     @Override
     public Iterator<Node<T>> iterator() {
-        // TODO Auto-generated method stub
-        return null;
+        List<Node<T>> it = new ArrayList<Node<T>>(nodes);
+        return it.iterator();
     }
 
     @Override
     public Iterator<Node<T>> heads() {
-        // TODO Auto-generated method stub
-        return null;
+        Iterator<Node<T>> it = iterator();
+        List<Node<T>> headsList = new ArrayList<Node<T>>();
+        while (it.hasNext()) {
+            Node<T> node = it.next();
+            if (node.isHead()) {
+                headsList.add(node);
+            }
+        }
+        return headsList.iterator();
     }
 
     @Override
     public int headCount() {
-        // TODO Auto-generated method stub
-        return 0;
+        int headCount = 0;
+        Iterator <Node<T>> it = heads();
+        while (it.hasNext()) {
+            Node<T> node = it.next();
+            if (node.isHead()) {
+                headCount++;
+            }
+            it.next();
+        }
+        return headCount;
     }
 
     @Override
     public Iterator<Node<T>> tails() {
-        // TODO Auto-generated method stub
-        return null;
+        Iterator<Node<T>> it = iterator();
+        List<Node<T>> tailList = new ArrayList<Node<T>>();
+        while (it.hasNext()) {
+            Node<T> node = it.next();
+            if (node.isTail()) {
+                tailList.add(node);
+            }
+        }
+        return tailList.iterator();
     }
 
     @Override
     public int tailCount() {
-        // TODO Auto-generated method stub
-        return 0;
+        int tailCount = 0;
+        Iterator <Node<T>> it = heads();
+        while (it.hasNext()) {
+            Node<T> node = it.next();
+            if (node.isTail()) {
+                tailCount++;
+            }
+            it.next();
+        }
+        return tailCount;
     }
 
     @Override
     public List<T> allItems() {
-        // TODO Auto-generated method stub
-        return null;
+        List<T> allItems = new ArrayList<T>();
+        Iterator<Node<T>> it = iterator();
+        while (it.hasNext()) {
+            allItems.add(it.next().item());
+        }
+        return allItems;
     }
 
     @Override
     public int edgeCount() {
-        // TODO Auto-generated method stub
-        return 0;
+        int edgeCount = 0;
+        Iterator<Node<T>> it = iterator();
+        while (it.hasNext()) {
+            edgeCount += it.next().inDegree();
+        }
+        return edgeCount;
     }
-
+    public static void main(String[] args) {
+        MyGraph<Integer> graph = new MyGraph<Integer>();
+        graph.addNodeFor(1);
+        graph.addNodeFor(2);
+        graph.addNodeFor(3);
+        graph.addNodeFor(4);
+        graph.addNodeFor(9999);
+        graph.addNodeFor(6);
+        graph.addNodeFor(7);
+        graph.addNodeFor(8);
+        graph.addNodeFor(9);
+        // graph.containsNodeFor(2);
+        System.out.println(graph.getNodeFor(9999));
+        System.out.println(graph.getNodeFor(1).getClass().getCanonicalName()); // magic
+    }
 }
