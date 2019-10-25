3ad90c84f368d81773b6066d4d786513286e2c5f
MosaKasem <thescorpion119@gmail.com>
2019-10-25 23:01:03 +0100

    cyclic complete

--- src/kr222if/graphs/impl/MyDFS.java
+++ src/kr222if/graphs/impl/MyDFS.java
@@ -1,6 +1,7 @@
 package kr222if.graphs.impl;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -11,8 +12,10 @@ import kr222if.graphs.Node;
 public class MyDFS<T> implements DFS<T> {
     List<Node<T>> visited;
     List<Node<T>> postOrder;
+    boolean cyclic = false;
 
     int counter;
+
     public MyDFS() {
         visited = new ArrayList<Node<T>>();
         postOrder = new ArrayList<Node<T>>();
@@ -44,6 +47,35 @@ public class MyDFS<T> implements DFS<T> {
         }
     }
 
+    public boolean dfsIsCyclic(Node<T> node) {
+        Iterator<Node<T>> succsIt = node.succsOf();
+        while (succsIt.hasNext()) {
+            Node<T> currentNode = succsIt.next();
+            Iterator<Node<T>> successorsSuccessor = currentNode.succsOf();
+
+            while (successorsSuccessor.hasNext()) {
+                if (successorsSuccessor.next().hasSucc(currentNode)) {
+                    cyclic = true;
+                    return true;
+                }
+            }
+        }
+        return false;
+/*         if (visited.contains(node)) {
+            cyclic = true;
+        }
+        visited.add(node); // Mark as visited
+        Iterator<Node<T>> succsIt = node.succsOf();
+        node.num = this.counter++;
+        while (succsIt.hasNext()) {
+            Node<T> successor = succsIt.next();
+            if (!visited.contains(successor)) {
+                dfsIsCyclic(successor);
+            }
+        } */
+        // return false;
+    }
+
     public List<Node<T>> dfsRecursive(Node<T> node) {
         visited.add(node); // Mark as visited
         Iterator<Node<T>> succsIt = node.succsOf();
@@ -57,9 +89,29 @@ public class MyDFS<T> implements DFS<T> {
         return visited;
     }
 
-    public List<Node<T>> postOrderRecursive(Node node) {
+    public List<Node<T>> postOrderRecursive(Node node, boolean attach_dfs_number) {
+        if (attach_dfs_number) {
+            node.num = this.counter++;
+        }
         Iterator<Node<T>> succsIt = node.succsOf();
         while (succsIt.hasNext()) {
+            Node<T> successor = succsIt.next();
+            if (!visited.contains(successor)) {
+                visited.add(node);
+                postOrderRecursive(successor);
+            }
+        }
+        if (!postOrder.contains(node) && attach_dfs_number != true) {
+            postOrder.add(node);
+            node.num = this.counter++;
+        }
+
+        return postOrder;
+    }
+
+    public List<Node<T>> postOrderRecursive(Node node) {
+        Iterator<Node<T>> succsIt = node.succsOf();
+        while (succsIt.hasNext()) { // 0(N*N)
             Node<T> successor = succsIt.next();
             if (!visited.contains(successor)) {
                 visited.add(node);
@@ -70,7 +122,7 @@ public class MyDFS<T> implements DFS<T> {
             postOrder.add(node);
             node.num = this.counter++;
         }
-    
+
         return postOrder;
     }
 
@@ -85,7 +137,7 @@ public class MyDFS<T> implements DFS<T> {
     public List<Node<T>> dfs(DirectedGraph<T> graph) {
         visited.clear();
         Iterator<Node<T>> graphIt = graph.iterator();
-        while (graphIt.hasNext()) {
+        while (graphIt.hasNext()) { // 0(N2)
             Node<T> node = graphIt.next();
             if (!visited.contains(node)) {
                 dfsRecursive(node);
@@ -119,20 +171,40 @@ public class MyDFS<T> implements DFS<T> {
 
     @Override
     public List<Node<T>> postOrder(DirectedGraph<T> g, boolean attach_dfs_number) {
-        // TODO Auto-generated method stub
-        return null;
+        visited.clear();
+        postOrder.clear();
+        this.counter = 0;
+        Iterator<Node<T>> graphIt = g.iterator();
+        while (graphIt.hasNext()) {
+            Node<T> node = graphIt.next();
+            if (!visited.contains(node)) {
+                postOrderRecursive(node, attach_dfs_number);
+            }
+        }
+        return postOrder;
     }
 
     @Override
     public boolean isCyclic(DirectedGraph<T> graph) {
-        // TODO Auto-generated method stub
-        return false;
+        cyclic = false;
+        visited.clear();
+        Iterator<Node<T>> graphIt = graph.iterator();
+        while (graphIt.hasNext()) {
+            Node<T> node = graphIt.next();
+            if (!visited.contains(node)) {
+                dfsIsCyclic(node);
+            } 
+        }
+        return cyclic;
     }
 
     @Override
     public List<Node<T>> topSort(DirectedGraph<T> graph) {
-        // TODO Auto-generated method stub
-        return null;
+        visited.clear();
+        postOrder.clear();
+        this.counter = 0;
+        Collections.reverse(postOrder(graph));
+        return postOrder;
     }
 
 }
